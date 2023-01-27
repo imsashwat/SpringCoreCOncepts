c738a90dc28ade76360d028041e25c858e29e57a
Shashwat Sharma <30342542+imsashwat@users.noreply.github.com>
2023-01-27 22:05:13 +0530

    loosely coupled kese bina person ko chede interfa

--- src/main/java/com/lcwd/core/SpringCoreCOncepts/SpringCoreCOnceptsApplication.java
+++ src/main/java/com/lcwd/core/SpringCoreCOncepts/SpringCoreCOnceptsApplication.java
@@ -3,6 +3,8 @@ package com.lcwd.core.SpringCoreCOncepts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lcwd.core.SpringCoreCOncepts.couple.Animal;
+import com.lcwd.core.SpringCoreCOncepts.couple.Dog;
 import com.lcwd.core.SpringCoreCOncepts.couple.Person;
 
 @SpringBootApplication
@@ -13,7 +15,11 @@ public class SpringCoreCOnceptsApplication {
 		// ye line springboot ko start krti h
 		SpringApplication.run(SpringCoreCOnceptsApplication.class, args);
 
-		Person p = new Person();
+		// loosely coupled
+		Animal animal = new Dog(); // interface me daal skte h child class
+		Person p = new Person(animal); // default constructor run
+		p.PlayWithAnimal();
+
 		// p.PlayWithAnimal();
 
 	}
